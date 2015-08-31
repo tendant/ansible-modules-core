2e405431a863215025a4c247fe90d30d54232b38
Lei <tendant@gmail.com>
2015-08-31 13:12:12 -0700

    Add github ID

--- cloud/amazon/ec2_vpc_peering.py
+++ cloud/amazon/ec2_vpc_peering.py
@@ -12,7 +12,7 @@ description:
     - This module has a dependency on python-boto
 version_added: "2.0"
 author:
-    - "Herby Gillot <herby.gillot@gmail.com>"
+    - "Herby Gillot (@herbygillot) <herby.gillot@gmail.com>"
     - "Lei Wang (@tendant)"
 notes:
     - You can specify VPCs via their VPC ID, CIDR block or tagged name.  If you use a CIDR or name and it matches more than one VPC, the module will abort, at which point you'd need to use something more specific (like the VPC's ID).
